38445abf5e500914f696d495edcbdb6f70bc1357
martinusgalih <martinus.galih@samudrainovasi.co.id>
2024-04-04 23:18:18 +0700

    Initial Commit

---
+++ The Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie
+//
+//  Created by Martinus Galih Widananto on 04/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
